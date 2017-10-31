a5861720635b383b943da3cabca6053486605b38
julianapinheiro <julianas.pinheiro@outlook.com>
2017-10-30 23:23:25 -0200

    EU ODEIO PROGRAMAR

---
+++ Prolog/T2/B/testes.pl
@@ -0,0 +1,23 @@
+%	:- set_prolog_flag(double_quotes, codes).
+%	:- initialization(testes).
+
+testes :-
+	consult('programa.pl'),
+	load,
+	cmd("pf 54 ge 37 pt 28 gd 95 pf 54 ge 37 pt 28 gd 95 pf 54
+	ge 37 pt 28 gd 95 pf 54 ge 37 pt 28 gd 95 pf 54 ge 37
+	pt 28 gd 95 pf 54 ge 37 pt 28 gd 95"),
+	cmd("un pt 200 ge 90 pf 200 ul"),
+	cmd("repita 36 [ gd 150 repita 8 [ pf 50 ge 45 ] ]"),
+	cmd("un pt 400 ul"),
+	cmd("repita 72 [ ge 10 pf 5 ]"), %foi
+	cmd("un pt 150 ul"),
+	cmd("repita 12 [ pf 100 gd 150 ]"), 
+	svg.
+
+teste1 :-
+	consult('programa.pl'),
+	load,
+	cmd("un pt 150 ul"),
+	cmd("repita 12 [ pf 100 gd 150 ]"), 
+	svg.
